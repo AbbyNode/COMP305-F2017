5c6f3ec8ee5d9e1ed67b3875b67c043a3485b88a
AbbyNode <AbbyNode@users.noreply.github.com>
2017-09-15 00:32:40 -0400

    Added shirt color changing
    Lab 2

---
+++ Assets/Scripts/ColorOnClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorOnClick : MonoBehaviour {
+	public SpriteRenderer spriteToModify;
+	public Color color;
+
+	private void OnMouseDown() {
+		spriteToModify.material.color = color;
+	}
+}
