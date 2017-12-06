b759a144d7dc6191d11255e1542cea8940cf4f19
AbbyNode <AbbyNode@users.noreply.github.com>
2017-12-06 10:06:59 -0500

    Started lab 5

---
+++ Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Bounds2D {
+	public Vector2 center;
+	public Vector2 extents;
+}
+
+public class ItemSpawner : MonoBehaviour {
+	public Bounds2D bounds;
+	public GameObject[] prefabs;
+	public int maxItems;
+	public float delay;
+
+	private float 
+
+	void Start() {
+
+	}
+
+	void Update() {
+
+	}
+}
